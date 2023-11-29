898c574bb0fbf246f6ace23f354d99642f90f992
Renuka Theminda Kirulapana <131508191+ThemindaKirulapana@users.noreply.github.com>
2023-11-29 23:45:24 +0530

    first commit

---
+++ process-form.php
@@ -0,0 +1,27 @@
+<!-- <?php
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+
+
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     // Retrieve form data
+//     $username = $_POST["Username"];
+//     $address = $_POST["Address"];
+//     $contactNumber = $_POST["contact Number"];
+//     $email = $_POST["email"];
+
+//     // Compose the email message
+//     $subject = "New Form Submission";
+//     $message = "Name: $username\n";
+//     $message .= "Address: $address\n";
+//     $message .= "Contact Number: $contactNumber\n";
+//     $message .= "Email: $email\n";
+
+//     // Send the email
+//     $to = "renuka.kirulapana@gmail.com"; // Replace with your email address
+//     $headers = "From: $email";
+
+//     // Use mail() function to send the email
+//     mail($to, $subject, $message, $headers);
+// }
+?> -->
